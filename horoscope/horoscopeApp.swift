7a20cfaf10f69aeaa629715da7b9d93433f636f1
gbpusddom6ci <gbpusddom6ci@gmail.com>
2026-02-25 02:06:59 +0300

    Initial Commit

---
+++ horoscope/horoscopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  horoscopeApp.swift
+//  horoscope
+//
+//  Created by malware on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct horoscopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ horoscopeTests/horoscopeTests.swift
@@ -0,0 +1,17 @@
+//
+//  horoscopeTests.swift
+//  horoscopeTests
+//
+//  Created by malware on 2/25/26.
+//
+
+import Testing
+@testable import horoscope
+
+struct horoscopeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
